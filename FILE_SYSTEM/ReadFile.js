1ee226d3474289aaf2d923fbcd8ddf353e7cf4a4
sundararajan-git <sundararajanselvarasu@gmail.com>
2024-03-19 21:45:56 +0530

    v1.0

---
+++ FILE_SYSTEM/ReadFile.js
@@ -0,0 +1,37 @@
+
+//  -------------- WITHOUT ES6 MODULES ------------------
+
+// CALL THE FILE MODULE
+const fs = require('fs')
+
+fs.readFile('./Text.txt', (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+})
+
+// CATCH THE ERROR 
+process.on('uncaughtException', err => {
+    console.error(`There was an uncaught error:  ${err}`)
+    process.exit(1)
+})
+
+
+
+
+// -------------------- WITH ES6 MODULES -------------------
+
+//  1. CHANGE THE EXTENTION NAME  .JS TO  .MJS  THEN RUN THE CODE  ( CMT - NODE FILENMAE.MJS )
+
+// import { readFile } from 'node:fs';
+
+
+// readFile('./Text.txt', (err, data) => {
+//     if (err) throw err;
+//     console.log(data.toString());
+// })
+
+
+// process.on('uncaughtException', err => {
+//     console.error(`There was an uncaught error:  ${err}`)
+//     process.exit(1)
+// })
